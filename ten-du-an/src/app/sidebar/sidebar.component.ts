35a5449f83e3e97bb03b4b5a5a048c8ec33088f3
Hiếu <haiphongtrhieu@gmail.com>
2023-07-31 08:17:40 +0700

    fix

---
+++ ten-du-an/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SidebarComponent {
+
+}
